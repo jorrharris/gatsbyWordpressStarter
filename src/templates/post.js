1f9c86a33b7789846aa65d3aa673c5592f2b796d
MSI\jorrh <jordanharris@cpp.edu>
2019-07-31 13:49:48 -0700

    Ready to deploy

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react"
+// import Img from "gatsby-image"
 
 class PostTemplate extends Component {
   render() {
     const post = this.props.data.wordpressPost
+    let resolutions
+    post.featured_media
+      ? (resolutions =
+          post.featured_media.localFile.childImageSharp.resolutions)
+      : (resolutions = null)
     console.log(post)
     return (
       <div className="page">
         <h1>{post.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        <h3>{post.acf.facebook}</h3>
+        <h3>{post.acf.twitter}</h3>
+        {resolutions && <img src={resolutions.src} alt="" />}
       </div>
     )
   }
@@ -20,6 +29,21 @@ export const pageQuery = graphql`
     wordpressPost(id: { eq: $id }) {
       title
       content
+      acf {
+        facebook
+        twitter
+      }
+      featured_media {
+        localFile {
+          childImageSharp {
+            resolutions(width: 300, height: 300) {
+              src
+              width
+              height
+            }
+          }
+        }
+      }
     }
   }
 `
